32d91f7c884e96f6dc4c9abe348bf18dd8d6c796
Chiyari <i@inn.moe>
2019-02-25 14:47:23 +0800

    Device & Main Form ADD

---
+++ Demo/Devices.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Demo
+{
+    partial class Devices
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.discover_device = new System.Windows.Forms.Button();
+            this.device_msg = new System.Windows.Forms.TextBox();
+            this.device_list = new System.Windows.Forms.ListBox();
+            this.return_main = new System.Windows.Forms.Button();
+            this.pair_device = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // discover_device
+            // 
+            this.discover_device.Location = new System.Drawing.Point(365, 20);
+            this.discover_device.Name = "discover_device";
+            this.discover_device.Size = new System.Drawing.Size(123, 33);
+            this.discover_device.TabIndex = 0;
+            this.discover_device.Text = "查找设备";
+            this.discover_device.UseVisualStyleBackColor = true;
+            this.discover_device.Click += new System.EventHandler(this.discover_device_Click);
+            // 
+            // device_msg
+            // 
+            this.device_msg.Location = new System.Drawing.Point(12, 150);
+            this.device_msg.Name = "device_msg";
+            this.device_msg.ReadOnly = true;
+            this.device_msg.Size = new System.Drawing.Size(347, 25);
+            this.device_msg.TabIndex = 1;
+            // 
+            // device_list
+            // 
+            this.device_list.FormattingEnabled = true;
+            this.device_list.ItemHeight = 15;
+            this.device_list.Location = new System.Drawing.Point(12, 20);
+            this.device_list.Name = "device_list";
+            this.device_list.Size = new System.Drawing.Size(347, 124);
+            this.device_list.TabIndex = 2;
+            // 
+            // return_main
+            // 
+            this.return_main.Location = new System.Drawing.Point(12, 181);
+            this.return_main.Name = "return_main";
+            this.return_main.Size = new System.Drawing.Size(123, 33);
+            this.return_main.TabIndex = 3;
+            this.return_main.Text = "返回主界面";
+            this.return_main.UseVisualStyleBackColor = true;
+            this.return_main.Click += new System.EventHandler(this.return_main_Click);
+            // 
+            // pair_device
+            // 
+            this.pair_device.Location = new System.Drawing.Point(365, 59);
+            this.pair_device.Name = "pair_device";
+            this.pair_device.Size = new System.Drawing.Size(123, 34);
+            this.pair_device.TabIndex = 4;
+            this.pair_device.Text = "设备配对";
+            this.pair_device.UseVisualStyleBackColor = true;
+            this.pair_device.Click += new System.EventHandler(this.pair_device_Click);
+            // 
+            // Devices
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pair_device);
+            this.Controls.Add(this.return_main);
+            this.Controls.Add(this.device_list);
+            this.Controls.Add(this.device_msg);
+            this.Controls.Add(this.discover_device);
+            this.Name = "Devices";
+            this.Text = "设备连接";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Devices_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button discover_device;
+        private System.Windows.Forms.TextBox device_msg;
+        private System.Windows.Forms.ListBox device_list;
+        private System.Windows.Forms.Button return_main;
+        private System.Windows.Forms.Button pair_device;
+    }
+}
---
+++ Demo/Devices.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+
+namespace Demo
+{
+    public partial class Devices : Form
+    {
+        public Form MainForm;
+
+        public Devices(Form form)
+        {
+            InitializeComponent();
+            MainForm = form;
+        }
+
+        BluetoothClient Blueclient = new BluetoothClient();
+        Dictionary<string, BluetoothAddress> deviceAddresses = new Dictionary<string, BluetoothAddress>();
+        string txtPwd = "0000";
+
+        public Devices()
+        {
+            InitializeComponent();
+        }
+
+        private void Devices_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (MainForm != null)
+                MainForm.Show();
+        }
+
+        private void return_main_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            if (MainForm != null)
+                MainForm.Show();
+        }
+
+        private void discover_device_Click(object sender, EventArgs e)
+        {
+            this.device_msg.Text = "";
+            this.device_msg.Visible = true;
+            BluetoothRadio BuleRadio = BluetoothRadio.PrimaryRadio;
+            BuleRadio.Mode = RadioMode.Connectable;
+            BluetoothDeviceInfo[] Devices = Blueclient.DiscoverDevices();
+            device_list.Items.Clear();
+            deviceAddresses.Clear();
+            foreach (BluetoothDeviceInfo device in Devices)
+            {
+                device_list.Items.Add(device.DeviceName);
+                deviceAddresses[device.DeviceName] = device.DeviceAddress;
+            }
+
+            this.device_msg.Text = "搜索设备完成,搜索到" + device_list.Items.Count + "个蓝牙设备。";
+        }
+
+        private void pair_device_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BluetoothAddress DeviceAddress = deviceAddresses[device_list.SelectedItem.ToString()];
+                Blueclient.SetPin(DeviceAddress, txtPwd.Trim());
+                Blueclient.Connect(DeviceAddress, BluetoothService.Handsfree);
+                MessageBox.Show("配对成功。");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
